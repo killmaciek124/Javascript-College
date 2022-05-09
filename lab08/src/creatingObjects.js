df7d44f33410d307be0d4c9c42f7278b881946a0
Maciej Maziuk <maciej@maziuk.com.pl>
2022-05-09 11:48:37 +0200

    zad1 w cO done

--- lab08/src/creatingObjects.js
+++ lab08/src/creatingObjects.js
@@ -5,94 +5,106 @@
 //  Zdefiniuj pola 'title' i 'author' i funkcję print(), która wypisze: author - title
 
 const book0 = {
-  // properties
+  title: "Karol pet",
+  author: "Mikey",
+  print: function print(){
+    console.log(this.author,"-",this.title) // this czyli element tego obiektu
+  }
 };
+
 
 // book0.print();
 
 const book1 = {};
-// properties
+Object.assign(book1, book0);
 
-// book1.print();
+//book1.print();
 
-const book2 = Object.create({});
-// properties
+const book2 = Object.create(book1);
+book2.title = "book2title"
+book2.author = "book2author"
 
-// book2.print();
+//book2.print();
 
 function BookCreator(title, author) {
-  const b = {};
-  // properties
+  const b = {}
+  b["title"] = title
+  b["author"] = author
+  b["print"] = function print(){
+    console.log(this.author,"-",this.title) // this czyli element tego obiektu
+  }
+  //console.log(b)
+  return b
 }
 
 const book3 = BookCreator("Cień wiatru", "Carlos Ruiz Zafon");
 const book4 = BookCreator("Ojciech Chrzestny", "Mario Puzo");
 
-// book3.print();
-// book4.print();
+book3.print();
+book4.print();
+// zad 1 DONE ! 
+// // ========================================
+// // ZADANIE 2
+// // ========================================
 
-// ========================================
-// ZADANIE 2
-// ========================================
+// // Przetestuj poniższy kod i odpowiedz na pytania
 
-// Przetestuj poniższy kod i odpowiedz na pytania
+// function testThis() {
+//   console.log(this);
+// }
 
-function testThis() {
-  console.log(this);
-}
+// testThis();
 
-testThis();
+// function testThis2() {
+//   "use strict";
+//   console.log(this);
+// }
 
-function testThis2() {
-  "use strict";
-  console.log(this);
-}
+// testThis2();
 
-testThis2();
+// // 2.1. Czym jest this?
+// // 2.2. Do czego odwołuje się this w obu przypadkach
 
-// 2.1. Czym jest this?
-// 2.2. Do czego odwołuje się this w obu przypadkach
+// const person = {
+//   name: "Oscar Wilde",
+//   print() {
+//     console.log(this.name);
 
-const person = {
-  name: "Oscar Wilde",
-  print() {
-    console.log(this.name);
+//     function a() {
+//       console.log(this);
+//     }
+//     a();
+//   },
+// };
+// person.print();
 
-    function a() {
-      console.log(this);
-    }
-    a();
-  },
-};
-person.print();
+// // 2.3. Jakie wartości przyjmuje this w powyższych przypadkach i dlaczego?
+// // 2.4. Zmodyfikuj powyższy kod w ten sposób, aby funkcja a wyświetlała w konsoli 'a: Oscar Wilde'. Nie używaj arrow function.
 
-// 2.3. Jakie wartości przyjmuje this w powyższych przypadkach i dlaczego?
-// 2.4. Zmodyfikuj powyższy kod w ten sposób, aby funkcja a wyświetlała w konsoli 'a: Oscar Wilde'. Nie używaj arrow function.
+// const printName = function () {
+//   console.log(this.name);
+// };
 
-const printName = function () {
-  console.log(this.name);
-};
+// const person1 = {
+//   name: "Aaron Towels",
+// };
 
-const person1 = {
-  name: "Aaron Towels",
-};
+// const person2 = {
+//   name: "Tom Clancy",
+// };
 
-const person2 = {
-  name: "Tom Clancy",
-};
+// // 2.5. Za pomocą funkcji printName wypisz 'name' obu autorów. Nie zmieniaj implementacji funkcji printName!
 
-// 2.5. Za pomocą funkcji printName wypisz 'name' obu autorów. Nie zmieniaj implementacji funkcji printName!
-
-const person3 = {
-  name: "Arthur Conan Doyle",
-  print() {
-    const a = () => {
-      console.log(this);
-    };
-    a();
-  },
-};
-person3.print();
+// const person3 = {
+//   name: "Arthur Conan Doyle",
+//   print() {
+//     const a = () => {
+//       console.log(this);
+//     };
+//     a();
+//   },
+// };
+// person3.print();
 
 // 2.6. Co wydrukuje w konsoli powyższy kod? Jaki scope ma arrow function?
 
