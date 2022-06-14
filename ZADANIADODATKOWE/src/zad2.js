911428fbc96971cf45d34a306d4e7c4186a11afa
Maciej Maziuk <maciej@maziuk.com.pl>
2022-06-14 11:43:01 +0200

    xd

--- ZADANIADODATKOWE/src/zad2.js
+++ ZADANIADODATKOWE/src/zad2.js
@@ -6,37 +6,41 @@ const func1 = () => {
    });
   }
   
-const func2 = () => {
-return new Promise( (resolve,reject) => {
-    setTimeout(() => {
-    resolve(2);
-},2000)
-  
-});
+const func2 = async () => {
+return 2
 }
+
+const func4 = async () => {
+    return 5
+    }
 
 const func3 = (x) => {
     return new Promise( (resolve,reject) => {
         setTimeout(() => {
         resolve(x*2);
-    },4000)
+    },1000)
       
     });
 }
 
-const funTab1 = [func1(),func2()];
+const funTab1 = [func1(),func2(),func4()];
 
 
 const connect = (funTab, fun) => {
-    Promise.all(funTab)
+    const res =[]
+    return Promise.all(funTab)
     .then(x=> {
-        async () => {
-            x.map(y=> [y, fun(y)])
-        }
-        console.log(x)
-    })
-    // .then(x=> console.log(x))
+        // console.log(x)
+            const y =x.map(y=>  fun(y))
+            return Promise.all([x,Promise.all(y)]).then(x=> {
+                // const res =[]
+                for (i=0; i<x[0].length;i++) {
+                    res.push([x[0][i], x[1][i]])
+                }
+                return res
+            })
 
+    })
 };
 
-connect(funTab1,func3)
+connect(funTab1,func3).then(x=> console.log(x))
